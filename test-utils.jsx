62a09f600f1862669e685fdfd1fca4fae06ee3c9
Sleman Kassis <slemankassis@hotmail.com>
2022-01-26 16:48:35 -0300

    test-utils

---
+++ test-utils.jsx
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+
+const AllTheProviders = ({ children }) => {
+  const clientSideEmotionCache = createEmotionCache();
+
+  return (
+          <div >{children}</div>
+  );
+};
+
+// jest.mock('react-i18next', () => ({
+//   // this mock makes sure any components using the translate hook can use it without a warning being shown
+//   useTranslation: () => {
+//     return {
+//       t: (str) => str,
+//       i18n: {
+//         changeLanguage: () => new Promise(() => {}),
+//       },
+//     };
+//   },
+// }));
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
